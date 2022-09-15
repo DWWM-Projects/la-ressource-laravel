e58780e4991de1a273af961c0ca7f4fc59d38144
Antrâxx <h.julien62@gmail.com>
2022-09-15 22:09:47 +0200

    Form front

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -8,10 +8,31 @@
     
     <h1 class="text-6xl text-center text-emerald-600 font-[Kalam] my-6">Nouvel article</h1>
 
-    <form action="" method="post" enctype="multipart/form-data">
+    <div class="w-1/4 mx-auto flex flex-col text-emerald-600">
 
-    </form>
+        <form action="" method="post" enctype="multipart/form-data">
 
+            <div class="flex flex-col border-4 border-green-400 rounded-lg text-2xl p-4 mb-3">
+                <label for="author">Votre pseudo</label>
+                <input type="text" name="author" id="author">
+            </div>
 
+            <div class="flex flex-col border-4 border-green-400 rounded-lg text-2xl p-4 mb-3">
+                <label for="content">Votre article</label>
+                <textarea name="content" id="content" cols="30" rows="10"></textarea>
+            </div>
+
+            <div class="flex flex-col border-4 border-green-400 rounded-lg text-2xl p-4 mb-3">
+                <label for="image">Image</label>
+                <input type="file" name="image" id="image">
+            </div>
+
+            <div class="text-center">
+                <button class="bg-green-400 hover:bg-green-600 hover:text-white duration-500 text-2xl rounded-lg p-2 mb-3">Poster</button>
+            </div>
+
+        </form>
+
+    </div>
 
 @endsection
